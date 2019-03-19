73260a31f088aa3838249eb33ea641e58adc3fb2
Roozbeh K <me@roozbeh.io>
2019-03-19 09:55:17 -0700

    Create _ivanti_software.ps1

---
+++ powershell/sql/_ivanti_software.ps1
@@ -0,0 +1,78 @@
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+cd $dir	
+
+
+
+function Invoke-Sqlcmd2 
+{ 
+    [CmdletBinding()] 
+    param( 
+    [Parameter(Position=0, Mandatory=$true)] [string]$ServerInstance, 
+    [Parameter(Position=1, Mandatory=$false)] [string]$Database, 
+    [Parameter(Position=2, Mandatory=$false)] [string]$Query, 
+    [Parameter(Position=3, Mandatory=$false)] [string]$Username, 
+    [Parameter(Position=4, Mandatory=$false)] [string]$Password, 
+    [Parameter(Position=5, Mandatory=$false)] [Int32]$QueryTimeout=600, 
+    [Parameter(Position=6, Mandatory=$false)] [Int32]$ConnectionTimeout=15, 
+    [Parameter(Position=7, Mandatory=$false)] [ValidateScript({test-path $_})] [string]$InputFile, 
+    [Parameter(Position=8, Mandatory=$false)] [ValidateSet("DataSet", "DataTable", "DataRow")] [string]$As="DataRow" 
+    ) 
+ 
+    if ($InputFile) 
+    { 
+        $filePath = $(resolve-path $InputFile).path 
+        $Query =  [System.IO.File]::ReadAllText("$filePath") 
+    } 
+ 
+    $conn=new-object System.Data.SqlClient.SQLConnection 
+      
+    if ($Username) 
+    { $ConnectionString = "Server={0};Database={1};User ID={2};Password={3};Trusted_Connection=False;Connect Timeout={4}" -f $ServerInstance,$Database,$Username,$Password,$ConnectionTimeout } 
+    else 
+    { $ConnectionString = "Server={0};Database={1};Integrated Security=True;Connect Timeout={2}" -f $ServerInstance,$Database,$ConnectionTimeout } 
+ 
+    $conn.ConnectionString=$ConnectionString 
+     
+    #Following EventHandler is used for PRINT and RAISERROR T-SQL statements. Executed when -Verbose parameter specified by caller 
+    if ($PSBoundParameters.Verbose) 
+    { 
+        $conn.FireInfoMessageEventOnUserErrors=$true 
+        $handler = [System.Data.SqlClient.SqlInfoMessageEventHandler] {Write-Verbose "$($_)"} 
+        $conn.add_InfoMessage($handler) 
+    } 
+     
+    $conn.Open() 
+    $cmd=new-object system.Data.SqlClient.SqlCommand($Query,$conn) 
+    $cmd.CommandTimeout=$QueryTimeout 
+    $ds=New-Object system.Data.DataSet 
+    $da=New-Object system.Data.SqlClient.SqlDataAdapter($cmd) 
+    [void]$da.fill($ds) 
+    $conn.Close() 
+    switch ($As) 
+    { 
+        'DataSet'   { Write-Output ($ds) } 
+        'DataTable' { Write-Output ($ds.Tables) } 
+        'DataRow'   { Write-Output ($ds.Tables[0]) } 
+    } 
+ 
+} #Invoke-Sqlcmd2
+
+# $query = Invoke-Sqlcmd2 -ServerInstance "BAHRAM\UPC" -Query "SELECT TOP 100 [AgentID],[HardwareClass],[HardwareName],[Quantity] FROM [PLUS].[dbo].[rpt_AgentHardware] where AgentID = '9f3bc8a5-cc48-49f8-a8cf-408329478332'"
+
+
+
+
+$AgentSoftwares = @()
+$AgentSoftwares = Invoke-Sqlcmd2 -ServerInstance "BAHRAM\UPC" -Query `
+"SELECT [AgentGUID],[Netbios],[Software]
+  FROM [PLUS].[dbo].[PLI_643_Agents] 
+  INNER JOIN  [PLUS].[dbo].[rpt_AgentSoftware] 
+  ON [PLUS].[dbo].[PLI_643_Agents].AgentGUID = [PLUS].[dbo].[rpt_AgentSoftware].AgentID"
+
+
+
+
+$AgentSoftwares | export-csv  -NoTypeInformation _ivanti_software.csv
+
