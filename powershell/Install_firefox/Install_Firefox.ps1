997f2a7190ed8b03a057bc6b726866e813df81ce
Roozbeh K <me@roozbeh.io>
2019-04-08 00:33:45 -0700

    Add files via upload

---
+++ powershell/Install_firefox/Install_Firefox.ps1
@@ -0,0 +1,24 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$path = Split-Path $scriptpath
+Set-Location  $path
+
+$computers = get-content ..\computers.txt
+
+
+$computers | where{test-connection $_ -quiet -count 1} | ForEach-Object{
+     
+
+     copy-item $path  "\\$_\c$\OS" -recurse -force
+
+    
+     Write-Host  "Application Installing on  $_  ..."
+     Invoke-Command -Computername $_ -ScriptBlock {
+     
+     	    Start-Process "C:\os\Install_Firefox\install.cmd" /silent -Wait
+
+               
+     }
+}
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
---
+++ powershell/Install_firefox/autoconfig.js
@@ -0,0 +1,3 @@
+// Student Affairs IT - Mozilla LockDown Settings
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.obscure_value", 0);
