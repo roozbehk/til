e5d3ef30a5e0864f9f9b5f427ba9243bdf1f334b
Roozbeh K <me@roozbeh.io>
2019-04-08 00:32:46 -0700

    Add files via upload

---
+++ powershell/Install_GoogleChrome/Install_GoogleChrome.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-File ""%~dpn0.ps1""' -Verb RunAs}"
---
+++ powershell/Install_GoogleChrome/Install_GoogleChrome.ps1
@@ -0,0 +1,26 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$path = Split-Path $scriptpath
+Set-Location  $path
+# Get Computer Names
+$computers = get-content ..\computers.txt
+
+# Chrome
+$installfile = "GoogleChromeStandaloneEnterprise64.msi"
+
+$computers | where{test-connection $_ -quiet -count 1} | ForEach-Object{
+     
+
+     copy-item $path  "\\$_\c$\os" -recurse -force
+
+     Write-Host  "Application Installing on  $_  ..."
+     Invoke-Command -Computername $_ -ScriptBlock {
+     
+      
+                  Start-Process "C:\os\Install_GoogleChrome\install.cmd" /silent -Wait
+
+               
+     }
+}
+
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
